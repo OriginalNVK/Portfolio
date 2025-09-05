96fead73d9a8a60175340f687ff191a3fc78a945
OriginalNVK <nguyenvankhanh27072004@gmail.com>
2025-09-05 16:00:09 +0700

    COmplete about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Navbar from "./components/Navbar";
 import { StarBackground } from "./components/StarBackground";
 import Home from "./pages/Home";
-
+import About from "./pages/About";
 
 function App() {
   return (
@@ -14,6 +14,19 @@ function App() {
         <Navbar />
         {/* Nội dung chính của trang */}
         <Home />
+        <About />
+        <footer>
+          <center>
+            <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
+            <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
+              © 2025{" "}
+              <a href="https://flowbite.com/" className="hover:underline">
+                OriginalNVK
+              </a>
+              . All Rights Reserved.
+            </span>
+          </center>
+        </footer>
       </div>
     </div>
   );
--- src/constant/index.js
+++ src/constant/index.js
@@ -4,3 +4,112 @@ export const navItems = [
     { name: "Showcase", href: "#Showcase" },
     { name: "Contact", href: "#Contact" }
 ];
+
+export const cert_list = [
+  {
+    id: 1,
+    name: "C# 12 - Beginner to Advanced",
+    link: "https://www.udemy.com/certificate/UC-581086a1-5b2e-4a01-ae4c-5fdb1ab1fd83/",
+  },
+  {
+    id: 2,
+    name: "ASP.NET Core Web API",
+    link: "https://www.udemy.com/certificate/UC-0609a1e4-d5dc-4da2-a977-cca931da8629/",
+  },
+  {
+    id: 3,
+    name: "C# basic Hackerrank",
+    link: "https://www.hackerrank.com/certificates/iframe/68c1a34d0cf9",
+  },
+  {
+    id: 4,
+    name: "Foundational C# with Microsoft",
+    link: "https://www.freecodecamp.org/certification/fcc4bc726cf-067f-4950-90e7-5983b9be80a0/foundational-c-sharp-with-microsoft",
+  },
+  {
+    id: 5,
+    name: "SQL advanced Hackerrank",
+    link: "https://www.hackerrank.com/certificates/iframe/676e87338923",
+  },
+  {
+    id: 6,
+    name: "Google UX Design",
+    link: "https://www.coursera.org/account/accomplishments/specialization/NK0CBAV63JVO",
+  },
+  {
+    id: 7,
+    name: "Frontend Development Libraries",
+    link: "https://www.freecodecamp.org/certification/fcc4bc726cf-067f-4950-90e7-5983b9be80a0/front-end-development-libraries?utm_source=chatgpt.com",
+  },
+  {
+    id: 8,
+    name: "Responsive Web Design",
+    link: "https://www.freecodecamp.org/certification/fcc4bc726cf-067f-4950-90e7-5983b9be80a0/responsive-web-design?utm_source=chatgpt.com",
+  },
+  {
+    id: 9,
+    name: "JavaScript Algorithms and Data Structures",
+    link: "https://www.freecodecamp.org/certification/fcc4bc726cf-067f-4950-90e7-5983b9be80a0/javascript-algorithms-and-data-structures-v8?utm_source=chatgpt.com",
+  },
+  {
+    id: 10,
+    name: "Foundations of Project Management",
+    link: "https://www.coursera.org/account/accomplishments/verify/CRZ0Y10LLA4T",
+  },
+  {
+    id: 11,
+    name: "Technical Support Fundamentals",
+    link: "https://www.coursera.org/account/accomplishments/verify/G2QPJ8VK5RDW",
+  },
+];
+
+export const proj_list = [
+  {
+    id: 1,
+    name: "HK Shop",
+    description:
+      "A personal web application developed to support the shopping experience for technology-related products. Built with C# using the ASP.NET MVC architecture, the application offers full functionality for both customers and administrators",
+    github: "https://github.com/OriginalNVK/ECommerce-HKShop",
+    live: "https://github.com/OriginalNVK/ECommerce-HKShop",
+  },
+  {
+    id: 2,
+    name: "Hotel Management",
+    description:
+      "Hotel Management app is a group project for Introductory SE course. Features include managing bookings, rooms, settings, invoices, and visualizing reports using React, TailwindCSS, ExpressJS, MS SQL.",
+    github: "https://github.com/OriginalNVK/Hotel-Management",
+    live: "https://hotel-management.enkay.tech/",
+  },
+  {
+    id: 3,
+    name: "School Management",
+    description:
+      "A Windows Forms application developed for internal data management and database security in an academic environment. The system supports user-role-based access control (RBAC), Oracle Label Security (OLS), fine-grained auditing, and recovery solutions.",
+    github: "https://github.com/OriginalNVK/School-Management",
+    live: "https://github.com/OriginalNVK/School-Management",
+  },
+  {
+    id: 4,
+    name: "Sushi Store Management",
+    description:
+      "A full-stack restaurant management system designed for a sushi restaurant chain. It supports multiple user roles, including customers, employees, branch managers, and company managers — providing seamless operations, from ordering and invoicing to feedback and reporting.",
+    github: "https://github.com/OriginalNVK/Sushi-Store-Management",
+    live: "https://github.com/OriginalNVK/Sushi-Store-Management",
+  },
+  {
+    id: 5,
+    name: "Modern Bank",
+    description:
+      "A visually striking and fully responsive banking website built with React and Tailwind CSS. This project represents a key milestone in my learning journey—showcasing not only my ability to design modern UIs but also to structure reusable components for scalability",
+    github: "https://github.com/OriginalNVK/Modern-Bank",
+    live: "https://github.com/OriginalNVK/Modern-Bank",
+  },
+  {
+    id: 6,
+    name: "AI Decision Tree",
+    description:
+      "This project implements decision tree algorithms for classification on multiple datasets, focusing on data preparation, algorithm implementation, evaluation, visualization, and comparison.",
+    github: "https://github.com/OriginalNVK/AI-Decision-Tree",
+    live: "https://github.com/OriginalNVK/AI-Decision-Tree",
+  },
+];
