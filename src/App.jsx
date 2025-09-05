c8e7421650a75f9299018eb0b770baf3d8ba995f
OriginalNVK <nguyenvankhanh27072004@gmail.com>
2025-09-05 14:33:35 +0700

    Complete home section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-function App() {
+import Navbar from "./components/Navbar";
+import { StarBackground } from "./components/StarBackground";
+import Home from "./pages/Home";
 
+
+function App() {
   return (
-    <h1 className="text-center text-3xl font-bold underline">Hello world!</h1>
+    <div className="min-h-screen relative">
+      {/* Star Background */}
+      <StarBackground />
+
+      {/* Main Content */}
+      <div className="relative z-10">
+        <Navbar />
+        {/* Nội dung chính của trang */}
+        <Home />
+      </div>
+    </div>
   );
 }
 
---
+++ src/constant/index.js
@@ -0,0 +1,6 @@
+export const navItems = [
+    { name: "Home", href: "#Home" },
+    { name: "About", href: "#About" },
+    { name: "Showcase", href: "#Showcase" },
+    { name: "Contact", href: "#Contact" }
+];
